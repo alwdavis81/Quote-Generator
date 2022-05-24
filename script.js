339fe1b0bcd9a96c0ff855f47f35d07adc359d8d
alwdavis81 <105399154+alwdavis81@users.noreply.github.com>
2022-05-24 00:28:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
+const twitterBtn = document
+  .getElementById('twitter')
+  .addEventListener('click', twitter);
 
 const newQuoteBtn = document
   .getElementById('new-quote')
   .addEventListener('click', newQuote);
+
 const loader = document.getElementById('loader');
 
 let apiQuotes = [];
